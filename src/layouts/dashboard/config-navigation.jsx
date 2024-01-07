244b2d176696d31c3219a06caf4c6d88e5d72fdd
Eniku Gumata <eniku32@gmail.com>
2024-01-07 17:08:03 +0300

    Update config-navigation.jsx

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -28,6 +28,11 @@ const navConfig = [
     icon: icon('ic_blog'),
   },
   {
+  title:'newTitle',
+  path:'',
+icon:icon('ic_blog'),
+},
+  {
     title: 'login',
     path: '/login',
     icon: icon('ic_lock'),

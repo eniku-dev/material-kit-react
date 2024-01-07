7ee634e8fb46cb4a3129f09b787514db3210cd75
Eniku Gumata <eniku32@gmail.com>
2024-01-07 17:11:16 +0300

    Update nav.jsx

--- src/layouts/dashboard/nav.jsx
+++ src/layouts/dashboard/nav.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 
 import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
